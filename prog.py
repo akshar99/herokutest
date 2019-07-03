3c2beebea778c9d79fcbac5d5d5ee6aa1b334929
akshar99 <42032147+akshar99@users.noreply.github.com>
2019-07-03 15:35:05 +0530

    prog.py

---
+++ prog.py
@@ -0,0 +1,15 @@
+import socket
+
+s = socket.socket()
+print("Socket successfully created")
+port = 11223
+s.bind(('', port))
+print("socket binded to %s" %port)
+s.listen(5)
+print("socket is listening")
+conn, addr = s.accept()
+with conn:
+    while True:
+        c, addr = s.accept()
+        print('Got connection from', addr)
+        c.close()
