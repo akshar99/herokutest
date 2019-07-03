d47f9a0a2685624b400c792cda96b0fd2548b055
akshar99 <42032147+akshar99@users.noreply.github.com>
2019-07-03 21:54:12 +0530

    Update server.py

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ import socket
 
 def server_program():
     host = socket.gethostname()
-    port = 5000 
+    port = 3005 
     server_socket = socket.socket()
     server_socket.bind((host, port))
     server_socket.listen(2)
